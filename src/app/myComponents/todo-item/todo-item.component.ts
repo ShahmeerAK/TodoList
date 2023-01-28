006fb981d75edf075612c2e89edd1f027cffc3f6
shahmeerak <shahmeer33ak@gmail.com>
2023-01-28 12:47:32 +0500

    Expand/Collapsed

--- src/app/myComponents/todo-item/todo-item.component.ts
+++ src/app/myComponents/todo-item/todo-item.component.ts
@@ -1,22 +1,26 @@
-import { Component,Input,Output,EventEmitter } from '@angular/core';
-import {Todo} from 'src/app/Todo';
+import { OnInit, Component, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent {
-@Input() todo!: Todo;
-@Input() i!:number;
-@Output() todoDelete: EventEmitter<Todo>=new EventEmitter();
-@Output() todoCheckBox: EventEmitter<Todo> = new EventEmitter();
+export class TodoItemComponent implements OnInit {
+  @Input() todo!: Todo;
+  @Input() i!: number;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckBox: EventEmitter<Todo> = new EventEmitter();
+  
+  public isCollapsed: boolean = false;
   checked = false;
   
-onClick(todo: Todo){
-  this.todoDelete.emit(todo);
-  console.log("Trigger");
-}
-onCheckBoxClick(todo:Todo){
-  this.todoCheckBox.emit(todo)
-}
+  ngOnInit() {
+  }
+  onClick(todo: Todo) {
+    this.todoDelete.emit(todo);
+    console.log("Trigger");
+  }
+  onCheckBoxClick(todo: Todo) {
+    this.todoCheckBox.emit(todo)
+  }
 }
